dfc522fcac3eb678a601ab663500eb79efecb110
ozavraham <ozavraham@10.30.100.109>
2018-03-19 12:20:37 +0200

    Lexer aviram changes

--- Oz1/src/Lexer.java
+++ Oz1/src/Lexer.java
@@ -1,8 +1,30 @@
-
+import java.util.*;
+import java.util.LinkedList;
 
 // Lexer divides input into tokens
 // function get_token gives the next token as string 
 
 public class Lexer {
-
+	LinkedList <Token> list = new LinkedList(); 
+	public Lexer (String str){
+		char tav;
+		for(int i = 0; i < str.length(); i++) {
+			tav = str.charAt(i);
+			if (tav >= 'a' && tav <= 'z') {
+				this.list.add(new Token(tav, TokenType.IDENTIFIER));
+			}
+			if (tav >= 0 && tav < 10) {
+				this.list.add(new Token(tav, TokenType.INTEGER));
+			}
+			if (tav == '+' || tav == '-' || tav == '*' || tav == '/' || tav == '='){
+				this.list.add(new Token(tav, TokenType.OPERNAD));
+			}
+			if ( tav == '(' || tav == ')') {
+				this.list.add(new Token(tav, TokenType.BRACKET));
+			}
+			if (tav == ';'){
+				this.list.add(new Token(tav, TokenType.END_OF_LINE));
+			}
+		}
+	}
 }
